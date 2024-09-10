c5a2087221fc832128085e35fa30cc3116cef946
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2024-09-10 17:04:46 +1200

    particle to be instantiated not called on play method

--- Assets/Scripts/CoinScript.cs
+++ Assets/Scripts/CoinScript.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinScript : MonoBehaviour {
+    [SerializeField] GameObject particleSystem;
     PlayerMovements playerMovement;
     void Start() {
-        playerMovement = FindObjectOfType<PlayerMovements>();
+        playerMovement = FindFirstObjectByType<PlayerMovements>();
     }
-
+    //Todo particle tobe spawned not played
     void OnTriggerEnter(Collider other) {
         if (other.gameObject.name == "Player") {
             playerMovement.Coin();
+            ParticleSystem[] particles =  particleSystem.GetComponentsInChildren<ParticleSystem>();
+            foreach (ParticleSystem particle in particles) {
+                particle.Play();
+                Debug.Log(particle.name);
+            }
         }
         Destroy(gameObject);
     }
